1fc48a1fd1c2400f85743db4e25ae45a2d71a8a6
Julien Chastang <julien.c.chastang@gmail.com>
2013-08-01 10:53:59 -0600

    first commit

---
+++ pythredds/__init__.py
@@ -0,0 +1,6 @@
+# use __init__.py to setup the namespace
+
+import util
+# will be able to call things in util using pythredds
+#    import pythredds
+#    pythredds.util
---
+++ pythredds/util/__init__.py
@@ -0,0 +1,9 @@
+from catalog_util import *
+#
+# This will pull in and expose the functions defined in the __all__
+#   list variable in catalog_util.py, and overall will work like this:
+#
+#    import pythredds
+#    dataset_url = "url goes here"
+#    access_url = pythredds.util.get_latest_dods_url(dataset_url)
+#
