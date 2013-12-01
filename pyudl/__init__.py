f1614d729f887ec723d5ad7974ee6984e23545c1
Julien Chastang <julien.c.chastang@gmail.com>
2013-12-01 03:29:24 -0700

    need to break out adde stuff into seperate project to prevent dependency creep

--- pyudl/__init__.py
+++ pyudl/__init__.py
@@ -1,7 +1,6 @@
 # use __init__.py to setup the namespace
 
 import tds
-import adde
 # will be able to call things in tds using pyudl
 #    import pyudl
 #    pyudl.tds
--- tests/catalog_util_test.py
+++ tests/catalog_util_test.py
@@ -1,21 +1,10 @@
 from pyudl.tds import get_latest_dods_url
-from pyudl.adde.projection import int_latlon_to_float
 import unittest
-
-# http://stackoverflow.com/a/3834356
-def near(a,b,rtol=1e-5,atol=1e-8):
-    # rtol is relative tolerance
-    # atol is absolute tolerance
-    try:
-        return abs(a-b)<(atol+rtol*abs(b))
-    except TypeError:
-        return False
 
 class Tests(unittest.TestCase):
 
     def test_example(self):
-        self.assertTrue(near( int_latlon_to_float(1563547),156.596388889))
-        self.assertTrue(near( int_latlon_to_float(0),0))
+        self.assertTrue(1 == 1)
 
 if __name__ == '__main__':
     unittest.main()
