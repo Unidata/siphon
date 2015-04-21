c0320766ae7dc4806afec12dd11ee8080f790988
Ryan May <rmay31@gmail.com>
2015-04-21 15:50:00 -0600

    Fix oops in test.

--- pyudl/tds/test/test_catalog.py
+++ pyudl/tds/test/test_catalog.py
@@ -5,12 +5,12 @@ class TestCatalog(object):
     baseURL = 'http://thredds-test.unidata.ucar.edu/thredds/'
 
     def test_basic(self):
-        url = baseURL + 'catalog.xml'
+        url = self.baseURL + 'catalog.xml'
         cat = TDSCatalog(url)
         assert 'Forecast Model Data' in cat.catalogRefs
 
     def test_access(self):
-        url = baseURL + 'catalog/grib/NCEP/GFS/Global_0p5deg/latest.xml'
+        url = self.baseURL + 'catalog/grib/NCEP/GFS/Global_0p5deg/latest.xml'
         cat = TDSCatalog(url)
         ds = list(cat.datasets.values())[0]
         assert 'OPENDAP' in ds.accessUrls
