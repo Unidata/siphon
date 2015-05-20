1c4bb74c45e20438c785eebff6617a648aa2ed52
Ryan May <rmay31@gmail.com>
2015-05-20 11:25:15 -0600

    Fix test_dods.

--- siphon/tds/test/test_dods.py
+++ siphon/tds/test/test_dods.py
@@ -6,6 +6,6 @@ from siphon.tds import get_latest_dods_url
 @vcr.use_cassette(os.path.join(os.path.dirname(__file__), 'fixtures/latest_rap_catalog'))
 def test_get_latest():
     url = ('http://thredds-test.unidata.ucar.edu/thredds/catalog/'
-           'grib/NCEP/RAP/CONUS_13km/latest.xml')
+           'grib/NCEP/RAP/CONUS_13km/catalog.xml')
     latest_url = get_latest_dods_url(url)
     assert latest_url
