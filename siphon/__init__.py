0405438080824b69517d9723031ce139cd058c69
Ryan May <rmay31@gmail.com>
2015-05-29 13:36:13 -0600

    Re-order __init__ so we can import __version__
    By setting __version__ first, other modules can pull it from there.

--- siphon/__init__.py
+++ siphon/__init__.py
@@ -1,7 +1,7 @@
-# use __init__.py to setup the namespace
-from . import cdmr
+# Version import needs to come first so everyone else can pull on import
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+from . import cdmr  # noqa
 __all__ = ['catalog', 'cdmr']
