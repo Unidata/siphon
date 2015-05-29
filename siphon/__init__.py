fc51259760c522593218b83b8c10ce4cf3f239db
Ryan May <rmay31@gmail.com>
2015-05-29 15:10:00 -0600

    Remove cdmr main level import.

--- siphon/__init__.py
+++ siphon/__init__.py
@@ -3,5 +3,4 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-from . import cdmr  # noqa
-__all__ = ['catalog', 'cdmr']
+__all__ = ['catalog', 'testing', 'util']
