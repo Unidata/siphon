73d0071bb48e13c5544ebc9d87b882fe276bcc42
Ryan May <rmay31@gmail.com>
2015-06-17 12:36:37 -0600

    Refactor CDMRemote to use HTTPEndPoint

--- siphon/cdmr/cdmremote.py
+++ siphon/cdmr/cdmremote.py
@@ -1,41 +1,29 @@
+from io import BytesIO
 from .ncstream import read_ncstream_messages
-from ..http_util import urlopen
+from ..http_util import HTTPEndPoint
 
 
-class CDMRemote(object):
-    # Create a custom url opener to add a user agent
-    def __init__(self, url):
-        self.url = url
-        self.responseHandler = read_ncstream_messages
-
-    def _fetch(self, url):
-        return self.responseHandler(urlopen(url))
+class CDMRemote(HTTPEndPoint):
+    def _fetch(self, query):
+        return read_ncstream_messages(BytesIO(self.get_query(query).content))
 
     def fetch_capabilities(self):
-        url = self.query_url(req='capabilities')
-        return self._fetch(url)
+        return self._fetch(self.query().add_query_parameter(req='capabilities'))
 
     def fetch_cdl(self):
-        url = self.query_url(req='CDL')
-        return self._fetch(url)
+        return self._fetch(self.query().add_query_parameter(req='CDL'))
 
     def fetch_data(self, **var):
         varstr = ','.join(name + self._convert_indices(ind)
                           for name, ind in var.items())
-        url = self.query_url(req='data', var=varstr)
-        return self._fetch(url)
+        query = self.query().add_query_parameter(req='data', var=varstr)
+        return self._fetch(query)
 
     def fetch_header(self):
-        url = self.query_url(req='header')
-        return self._fetch(url)
+        return self._fetch(self.query().add_query_parameter(req='header'))
 
     def fetch_ncml(self):
-        url = self.query_url(req='NcML')
-        return self._fetch(url)
-
-    def query_url(self, **kw):
-        query = '&'.join('%s=%s' % i for i in kw.items())
-        return '?'.join((self.url, query))
+        return self._fetch(self.query().add_query_parameter(req='NcML'))
 
     @staticmethod
     def _convert_indices(ind):
