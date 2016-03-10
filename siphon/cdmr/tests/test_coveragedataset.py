c2b226248ff30ab0b981a7675a4b4669c395aaa3
Ryan May <rmay31@gmail.com>
2016-03-10 16:00:08 -0700

    Add basic class to open dataset over CDMRemoteFeature.
    This is very raw at the moment. Decoding more of the protobuf still
    needs some more thought.

---
+++ siphon/cdmr/tests/test_coveragedataset.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2016 Unidata.
+# Distributed under the terms of the MIT License.
+# SPDX-License-Identifier: MIT
+import warnings
+
+from siphon.testing import get_recorder
+from siphon.cdmr.coveragedataset import CoverageDataset
+
+recorder = get_recorder(__file__)
+
+# Ignore warnings about CoverageDataset
+warnings.simplefilter('ignore')
+
+
+@recorder.use_cassette('hrrr_cdmremotefeature')
+def test_simple_cdmremotefeature():
+    'Just a smoke test for CDMRemoteFeature'
+    cd = CoverageDataset('http://localhost:8080/thredds/cdmrfeature/grid/'
+                         'test/HRRR_CONUS_2p5km_20160309_1600.grib2')
+    assert cd.grids
