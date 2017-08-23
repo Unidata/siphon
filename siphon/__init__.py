ba1ad9191600b3319b520cd8d9a0323bc58ba808
J.R. Leeman <kd5wxb@gmail.com>
2017-08-23 09:16:49 -0600

    Add Wyoming upper air capability.

--- siphon/__init__.py
+++ siphon/__init__.py
@@ -8,4 +8,4 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-__all__ = ['catalog', 'testing', 'http_util']
+__all__ = ['catalog', 'testing', 'http_util', 'upperair']
