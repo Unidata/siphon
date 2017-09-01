0380b80bab483a7ffd09a28cf4367f9b33bc7784
Ryan May <rmay@ucar.edu>
2017-09-01 15:48:27 -0600

    ENH: Set a link to the canonical version of the docs
    This will help search engines favor the latest version

--- docs/conf.py
+++ docs/conf.py
@@ -147,7 +147,7 @@ def setup(app):
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {'canonical_url': 'https://unidata.github.io/siphon/latest/'}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
