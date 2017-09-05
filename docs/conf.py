0e639e998809040f04b0b073ad163e651e1de0ee
Ryan May <rmay@ucar.edu>
2017-09-05 13:08:54 -0600

    MNT: Add datestamp to docs

--- docs/conf.py
+++ docs/conf.py
@@ -98,7 +98,7 @@ release = verinfo
 # non-false value, then it is used:
 #today = ''
 # Else, today_fmt is used as the format for a strftime call.
-#today_fmt = '%B %d, %Y'
+today_fmt = '%B %d, %Y'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
