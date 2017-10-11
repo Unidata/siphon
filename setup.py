19eb1ea8c188706af2c6d209050007d4c26b8f78
J.R. Leeman <kd5wxb@gmail.com>
2017-10-11 10:15:57 -0600

    [MNT] Do not allow sphinx 1.6.4

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
                  'flake8-mutable', 'flake8-pep3101', 'flake8-print', 'flake8-quotes',
                  'pep8-naming',
                  'vcrpy~=1.5,!=1.7.0,!=1.7.1,!=1.7.2,!=1.7.3', 'xarray>=0.6'],
-        'doc': ['sphinx>=1.3', 'sphinx-gallery', 'doc8'],
+        'doc': ['sphinx>=1.3,!=1.6.4', 'sphinx-gallery', 'doc8'],
         # SciPy needed for cartopy; we don't use cartopy[plotting] because
         # that will pull in GDAL.
         'examples': ['matplotlib>=1.3', 'cartopy>=0.13.1', 'scipy']
