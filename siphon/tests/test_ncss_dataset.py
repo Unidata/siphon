d33d8ad597f59e6798638d3fa7d3de9321e1b05d
Ryan May <rmay@ucar.edu>
2017-11-02 22:21:56 -0600

    MNT: Add test for parsing TDS 5.0 dataset.xml

--- siphon/tests/test_ncss_dataset.py
+++ siphon/tests/test_ncss_dataset.py
@@ -573,3 +573,13 @@ def test_dataset_elements_full_ncss_grid():
     element = ET.fromstring(urlopen(url).read())
     parsed = NCSSDataset(element)
     assert parsed
+
+
+@recorder.use_cassette('GFS_TDS5')
+def test_dataset_parsing_tds5(recwarn):
+    """Test parsing the dataset from TDS 5."""
+    url = ('http://thredds-test.unidata.ucar.edu/thredds/ncss/grid/casestudies/irma/model/'
+           'gfs/GFS_Global_0p5deg_20170903_1200.grib2/dataset.xml')
+    element = ET.fromstring(urlopen(url).read())
+    NCSSDataset(element)
+    assert len(recwarn) == 0
