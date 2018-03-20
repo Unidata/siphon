65d68424445fd06cf2a15b8348f7e77c00acbfe3
Ryan May <rmay@ucar.edu>
2018-03-19 20:57:18 -0600

    MNT: Fix with xarray 0.10.2 (Fixes #192)
    XArray reworked some of the classes that support our backend, so we need
    to update. This also means that this (admittedly under exposed) code
    only works with xarray >=0.10.2, so bump the requirements for it.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
                  'flake8-copyright', 'flake8-docstrings', 'flake8-import-order',
                  'flake8-mutable', 'flake8-pep3101', 'flake8-print', 'flake8-quotes',
                  'pep8-naming',
-                 'vcrpy~=1.5,!=1.7.0,!=1.7.1,!=1.7.2,!=1.7.3', 'xarray>=0.6'],
+                 'vcrpy~=1.5,!=1.7.0,!=1.7.1,!=1.7.2,!=1.7.3', 'xarray>=0.10.2'],
         'doc': ['sphinx>=1.3,!=1.6.4', 'sphinx-gallery', 'doc8', 'recommonmark'],
         # SciPy needed for cartopy; we don't use cartopy[plotting] because
         # that will pull in GDAL.
