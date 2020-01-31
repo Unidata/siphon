6f7f409ea32bb72d4060bbd5c271c893332dd4ff
Spencer Riley <spencer.riley@student.nmt.edu>
2020-01-31 12:26:51 -0700

    Fixed workarounds

--- siphon/http_util.py
+++ siphon/http_util.py
@@ -18,7 +18,7 @@ except ImportError:
 
 import requests
 
-# from . import __version__
+from . import __version__
 
 HTTPError = requests.HTTPError
 
@@ -53,7 +53,7 @@ class HTTPSessionManager(object):
 
     def __init__(self):
         """Initialize ``HTTPSessionManager``."""
-        self.user_agent = 'Siphon ({})'.format("1")
+        self.user_agent = 'Siphon ({})'.format(__version__)
         self.options = {}
 
     def set_session_options(self, **kwargs):
