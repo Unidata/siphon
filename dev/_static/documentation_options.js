a81be4b0b3ef357543e0adeb1ac1e6389e334b7e
dopplershift <dopplershift@users.noreply.github.com>
2021-03-11 20:14:40 +0000

    Deploy dev to GitHub Pages

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9+6.ga311f3f',
+    VERSION: '0.9+8.gd7f801c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
