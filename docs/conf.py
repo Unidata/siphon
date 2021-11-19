59ef12d57bfd5d76e4820e191fcfa3d09ea9eece
Ryan May <rmay@ucar.edu>
2021-11-19 00:10:54 -0700

    DOC: Update sphinx config
    Replace deprecated call to `add_stylesheet` with a function that
    apparently has been around since 1.0.

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '2.1'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -140,7 +140,7 @@ except ImportError:
     pass
 
 def setup(app):
-        app.add_stylesheet('theme_override.css')
+        app.add_css_file('theme_override.css')
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
