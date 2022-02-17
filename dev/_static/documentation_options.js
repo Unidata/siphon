e21870e1e85b07ac719188e573abeae6af041b5f
dcamron <dcamron@users.noreply.github.com>
2022-02-17 17:24:47 +0000

    Deploy dev to GitHub Pages

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.post196+ga4e2893',
+    VERSION: '0.9.post198+g7ff2c05',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
