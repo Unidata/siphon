119f64fa61cd4adfb25cba4e24fe1df119a78d21
Ryan May <rmay@ucar.edu>
2026-07-15 16:18:35 -0600

    ENH: Update WyomingUpperAir client
    The University of Wyoming changed the server and software server, so
    update the client to match. This simplifies things since we can now
    directly request data as CSV.

--- src/siphon/simplewebservice/wyoming.py
+++ src/siphon/simplewebservice/wyoming.py
@@ -3,21 +3,15 @@
 # SPDX-License-Identifier: BSD-3-Clause
 """Read upper air data from the Wyoming archives."""
 
-from datetime import datetime
 from io import StringIO
 import warnings
 
-from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
+import requests
 
 from .._tools import get_wind_components
 from ..http_util import HTTPEndPoint
-
-
-def _safe_float(s):
-    """Convert to float, handling ****** as a string for missing."""
-    return pd.NA if all(c == '*' for c in s) or s == '-9999.0' else float(s)
 
 
 class WyomingUpperAir(HTTPEndPoint):
@@ -25,7 +19,7 @@ class WyomingUpperAir(HTTPEndPoint):
 
     def __init__(self):
         """Set up endpoint."""
-        super().__init__('http://weather.uwyo.edu/cgi-bin/sounding')
+        super().__init__('http://weather.uwyo.edu/wsgi')
 
     @classmethod
     def request_data(cls, time, site_id, recalc=False, **kwargs):
@@ -79,41 +73,26 @@ class WyomingUpperAir(HTTPEndPoint):
 
         """
         raw_data = self._get_data_raw(time, site_id, recalc=recalc)
-        soup = BeautifulSoup(raw_data, 'html.parser')
-        tabular_data = StringIO(soup.find_all('pre')[0].contents[0])
-        col_names = ['pressure', 'height', 'temperature', 'dewpoint', 'direction', 'speed']
-        df = pd.read_fwf(tabular_data, widths=[7] * 8, skiprows=5,
-                         usecols=[0, 1, 2, 3, 6, 7], names=col_names)
+
+        # Parse CSV. skipinitialspace is used to handle missing data being encoded by just
+        # having spaces in the field
+        df = pd.read_csv(StringIO(raw_data), parse_dates=['time'],
+                         date_format='%Y-%m-%d %H:%M:%S', skipinitialspace=True,
+                         na_values={'latitude':'-99.9900', 'longitude':'-99.9900'})
+        df = df.rename(columns=lambda c: c.split('_')[0])
+        df = df.rename(columns=lambda c: c.replace(' ', '_'))
+        df = df.rename(columns={'geopotential_height': 'height',
+                                'dew_point_temperature': 'dewpoint',
+                                'wind_direction': 'direction', 'wind_speed': 'speed'})
+
+        # Drop any rows with all NaN values for T, Td, winds
+        df = df.dropna(subset=('temperature', 'dewpoint', 'direction', 'speed'),
+                       how='all').reset_index(drop=True)
 
         df['u_wind'], df['v_wind'] = get_wind_components(df['speed'],
                                                          np.deg2rad(df['direction']))
-
-        # Drop any rows with all NaN values for T, Td, winds
-        df = df.dropna(subset=('temperature', 'dewpoint', 'direction', 'speed',
-                               'u_wind', 'v_wind'), how='all').reset_index(drop=True)
-
-        # Parse metadata
-        meta_data = soup.find_all('pre')[1].contents[0]
-        lines = meta_data.splitlines()
-
-        # Convert values after table into key, value pairs using the name to the left of the :
-        post_values = dict(tuple(map(str.strip, line.split(': '))) for line in lines[1:])
-
-        station = post_values.get('Station identifier', '')
-        station_number = int(post_values['Station number'])
-        sounding_time = datetime.strptime(post_values['Observation time'], '%y%m%d/%H%M')
-        latitude = _safe_float(post_values.get('Station latitude', '******'))
-        longitude = _safe_float(post_values.get('Station longitude', '******'))
-        elevation = _safe_float(post_values.get('Station elevation', '******'))
-        pw = float(post_values['Precipitable water [mm] for entire sounding'])
-
-        df['station'] = station
-        df['station_number'] = station_number
-        df['time'] = sounding_time
-        df['latitude'] = latitude
-        df['longitude'] = longitude
-        df['elevation'] = elevation
-        df['pw'] = pw
+        df['station'] = site_id
+        df['height'] = df['height'].astype('float')
 
         # Add unit dictionary
         with warnings.catch_warnings():
@@ -124,16 +103,13 @@ class WyomingUpperAir(HTTPEndPoint):
                         'temperature': 'degC',
                         'dewpoint': 'degC',
                         'direction': 'degrees',
-                        'speed': 'knot',
-                        'u_wind': 'knot',
-                        'v_wind': 'knot',
+                        'speed': 'm/s',
+                        'u_wind': 'm/s',
+                        'v_wind': 'm/s',
                         'station': None,
-                        'station_number': None,
                         'time': None,
                         'latitude': 'degrees',
-                        'longitude': 'degrees',
-                        'elevation': 'meter',
-                        'pw': 'millimeter'}
+                        'longitude': 'degrees'}
 
         return df
 
@@ -155,17 +131,11 @@ class WyomingUpperAir(HTTPEndPoint):
             text of the server response
 
         """
-        path = ('?region=naconf&TYPE=TEXT%3ALIST'
-                f'&YEAR={time:%Y}&MONTH={time:%m}&FROM={time:%d%H}&TO={time:%d%H}'
-                f'&STNM={site_id}')
+        path = f'sounding?type=TEXT%3ACSV&datetime={time:%Y-%m-%d %H:%M:%S}&id={site_id}'
         if recalc:
             path += '&REPLOT=1'
 
-        resp = self.get_path(path)
-        # See if the return is valid, but has no data
-        if resp.text.find("Can't") != -1:
-            raise ValueError(
-                f'No data available for {time:%Y-%m-%d %HZ} '
-                f'for station {site_id}.')
-
-        return resp.text
+        try:
+            return self.get_path(path).text
+        except requests.HTTPError as e:
+            raise ValueError from e
